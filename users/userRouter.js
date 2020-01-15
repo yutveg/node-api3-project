07cf708f619db049a4763dd2acc522b7bc3cf28a
yutveg <kodi.0n31l@gmail.com>
2020-01-15 13:44:28 -0800

    user endpoints built

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,69 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
+const Users = require("./userDb.js");
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post("/", (req, res) => {
+  Users.insert(req.body)
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Something has gone wrong." });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post("/:id/posts", (req, res) => {
+  Posts.insert(req.body)
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Something has gone wrong." });
+    });
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get("/", (req, res) => {
+  Users.get()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Something has gone wrong." });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get("/:id", (req, res) => {
+  Users.getById(req.params.id)
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Something has gone wrong." });
+    });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get("/:id/posts", (req, res) => {
+  Users.getById(req.params.id)
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete("/:id", (req, res) => {
+  Users.remove(req.params.id)
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
